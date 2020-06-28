87c6c6cbea2ec95753d30c30cb38db521443c8b6
kangkai <kangkai_ts@hotmail.com>
2020-06-28 19:04:45 +0800

    --项目结构调整

---
+++ src/main/java/com/bitwave/entity/SysUser.java
@@ -0,0 +1,52 @@
+package com.bitwave.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * (SysUser)实体类
+ *
+ * @author makejava
+ * @since 2020-06-23 16:37:11
+ */
+
+@Getter
+@Setter
+@TableName("sys_user")
+public class SysUser implements Serializable {
+
+    private static final long serialVersionUID = -48537488874498249L;
+    
+    private Integer id;
+    /**
+    * 用户名
+    */
+    private String username;
+    /**
+    * 密码
+    */
+    private String password;
+    /**
+    * 显示名称
+    */
+    private String name;
+    /**
+    * 创建日期
+    */
+
+    private Date createdate;
+    /**
+    * 更新日期
+    */
+    private Date updatedate;
+    /**
+    * 状态码（0:有效 1:冻结  2:永久禁用）
+    */
+    private Short state;
+
+
+}
