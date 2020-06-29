302808471818e58da3396521ebf6f9de4167d99c
kangkai <kangkai_ts@hotmail.com>
2020-06-29 16:21:48 +0800

    -- 重构

--- src/main/java/com/bitwave/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.bitwave.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.bitwave.entity.SysUser;
-
-public interface UserMapper extends BaseMapper<SysUser> {
-
-}
